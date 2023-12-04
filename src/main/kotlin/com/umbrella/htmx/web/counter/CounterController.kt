a31f042c328f27dcba58be5084c5d45888fa848c
Felix Eckhardt <felix.e@gmx.de>
2023-12-04 11:22:10 +0100

    update formatting, few tweaks

--- src/main/kotlin/com/umbrella/htmx/web/counter/CounterController.kt
+++ src/main/kotlin/com/umbrella/htmx/web/counter/CounterController.kt
@@ -5,16 +5,15 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/counter")
 class CounterController {
 
     @GetMapping("/count")
-    fun count(model: Model, request: HttpServletRequest, response: HttpServletResponse): String {
-        val count = request.getHeader("query_count")?.let { it.toInt() } ?: 0;
+    fun count(@RequestHeader("query_count") count: Int = 0, model: Model, request: HttpServletRequest, response: HttpServletResponse): String {
         val newCount = count + 1
 
         // set count for template
